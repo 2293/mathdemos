6905517c7cbf8eec9297f1753c23c6b26348c7d5
a-boy <avvboy@gmail.com>
2026-09-18 13:18:18 +0800

    20260918删后重建

---
+++ 000003solve_3n+1_problem/tree.js
@@ -0,0 +1,76 @@
+// demo data
+var data = {
+  name: 1,
+  children: [
+    { name: 1 },
+    {
+      name: 5, children: [
+        { name: 3 },
+        {
+          name: 13, children: [
+            { name: 17 },
+            { name: 69 }
+          ]
+        }]
+    },
+    { name: 21 },
+    { name: 85 }
+  ]
+}
+
+// define the item component
+Vue.component('item', {
+  template: '#item-template',
+  props: {
+    model: Object
+  },
+  data: function () {
+    return {
+      open: true
+    }
+  },
+  computed: {
+    isFolder: function () {
+      return this.model.children &&
+        this.model.children.length
+    }
+  },
+  methods: {
+    toggle: function () {
+      if (this.isFolder) {
+        this.open = !this.open
+      }
+    },
+    changeType: function () {
+      if (!this.isFolder && (+this.model.name % 3 != 0)) {
+        Vue.set(this.model, 'children', [])
+        this.addChild()
+        this.open = true
+      }
+    },
+    addSibling: function () {
+      last = +this.model.children[this.model.children.length - 1].name;
+      console.log(h(last));
+      this.model.children.push({ name: h(last) });
+    },
+    addChild: function () {
+      console.log(this.model.name);
+      if (+this.model.name % 3 != 0) {
+        this.model.children.push(
+          { name: v(+this.model.name) },
+          { name: h(v(+this.model.name)) },
+          { name: h(h(v(+this.model.name))) },
+          { name: h(h(h(v(+this.model.name)))) }
+        )
+      }
+    }
+  }
+})
+
+// boot up the demo
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    treeData: data
+  }
+})
---
+++ README.md
@@ -0,0 +1,22 @@
+math demos with sagecell 数学演示，以sagemath/sagecell驱动
+
+0 Foundations of Mathematics | 1 Number Theory | 2 Discrete Mathematics | 3 Algebra |
+4 Geometry | 5 Calculus and Analysis | 6 Applied Mathematics | 7 Probability and Statistics |
+8 Topology | 9 History and Terminology | 10 Recreational Mathematics | 11 misc
+
+<ul>{% for file in site.static_files %}{% if file.extname == ".html" %}
+<li><a href="{{file.path | replace_first: '/', ''}}">{{file.path | replace_first: '/', ''}}</a></li>{% endif %}{% endfor %}
+</ul>
+<ul>
+  {% for page in site.pages %}
+  <li><a href="{{ page.url | relative_url }}">{{ page.url }}</a></li>
+  {% endfor %}
+</ul>
+<ul>
+{% for post in site.posts %}	
+    <li><a href="{{ post.url | relative_url }}">{{ post.title }}</a><small><strong>{{ post.date | date: "%B %e, %Y" }}</strong> . {{ post.category }} </small></li>	
+{% endfor %}
+</ul>
+
+![mathdemos](logo.png)
+<h5><a href="https://github.com/2293/mathdemos/new/main">Contribute to this site</a></h5>
